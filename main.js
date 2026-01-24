78c830ed937777789ea0e90b7c7e1e50c59ec2ea
lamchiwing <60570944+lamchiwing@users.noreply.github.com>
2026-01-25 00:38:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
   // ===== CONFIG =====
   // Your Apps Script Web App URL (exec)
   const WEB_APP_BASE =
-    "https://script.google.com/macros/s/AKfycbwpsL4RKmIOFkVIxqdGfT4FxaH2Fp3zmNrCY2_cgrsMiO4jblAj_oEFNYNOrPQuU9ZJeA/exec";
+    "https://script.google.com/macros/s/AKfycbwpsL4RKmIOFkVIxqdGfT4FxaH2Fp3zmNrCY2_cgrsMiO4jblAj_oEFNYNOrPQuU9ZJeA/exect?=leactrent";
 
   // Default tenant: from meta tag <meta name="leact-tenant-default" content="leactrent">
   function getDefaultTenant() {
